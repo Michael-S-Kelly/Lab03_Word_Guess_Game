9079bae53378c5d55e341e33c3993f4e6910bc66
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-22 16:02:36 -0700

    Completed the display words function

---
+++ ReadMe.md
@@ -0,0 +1,70 @@
+# Lab03-Word Guessing Game
+
+------------------------------
+
+# Word Guessing Game
+#### Lab03-Word Guessing Game
+##### *Author: Mike Kelly*
+
+------------------------------
+
+## Description
+This C# program is a word guessing game on the console.  It has the user guess a word 1 letter at a time.  It also allows the user to add and remove words from the list.
+
+------------------------------
+
+## Getting Started
+Clone this repository to your local machine.
+```
+$ git clone [https://github.com/Michael-S-Kelly/Lab03_Word_Guess_Game.git]
+```
+#### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the ```WordGuessingGame``` directory.
+
+Then select and open ```WordGuessingGame.sln```
+
+------------------------------
+
+## Visuals
+
+
+##### Application Start
+![Image 1](Assets/Begin.PNG)
+##### Checking Balance
+![Image 2](Assets/CheckBal.PNG)
+##### First Withdrawal of the session
+![Image 3](Assets/FirstWith.PNG)
+##### Withdrawals after the first
+![Image 4](Assets/AfterWith.PNG)
+##### First Deposit of the session
+![Image 5](Assets/firstDep.PNG)
+##### Deposits after the first
+![Image 6](Assets/AfterDep.PNG)
+##### Application End
+![Image 6](Assets/end.PNG)
+
+------------------------------
+
+## Change Log
+
+
+
+
+------------------------------
+## Collaborators, Contributors, and Other Resources used
+
+### Collaborators
+
+### Contributors
+####Phil Werner
+
+
+### Other Resources
+####https://stackoverflow.com/questions/12633815/lines-of-a-streamreader-to-an-array-of-strring
+
+------------------------------
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
--- WordGuessingGame/WordGuessingGame/Program.cs
+++ WordGuessingGame/WordGuessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WordGuessingGame
 {
@@ -14,6 +15,10 @@ namespace WordGuessingGame
         }
 
         #region Main Menu
+        /// <summary>
+        /// This is method creates the main navigational menu.
+        /// </summary>
+        /// <returns>returns true if the user staying in the program or false to exit the program</returns>
         static bool MainMenu()
         {
             Console.Clear();
@@ -31,6 +36,8 @@ namespace WordGuessingGame
                     return true;
                 } else if (choice == "2")
                 {
+                    Console.Clear();
+                    Options();
                     return true;
                 } else if (choice == "3")
                 {
@@ -62,6 +69,120 @@ namespace WordGuessingGame
         #region Game
         #endregion
         #region Options Menu
+        static void Options()
+        {
+            bool menuReturn = true;
+            while (menuReturn)
+            {
+                menuReturn = OptionsMenu();
+            }
+        }
+        static bool OptionsMenu()
+        {
+            Console.WriteLine("Please enter the number for one of the following options.");
+            Console.WriteLine("1) Display a list of the words");
+            Console.WriteLine("2) Add a word");
+            Console.WriteLine("3) Delete a word");
+            Console.WriteLine("4) Exit Options Menu");
+
+            string path = "../../../wordlist.txt";
+
+            try
+            {
+                CreateFile(path);
+
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    DisplayWordList(path);
+
+                    return true;
+                }
+                else if (choice == "2")
+                {
+                    AddWord(path);
+                    return true;
+                }
+                else if (choice == "3")
+                {
+                    return true;
+                }
+                else if (choice == "4")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is not a leginimate option.");
+                    Console.WriteLine("Please press enter to try again.");
+                    Console.ReadLine();
+                    return true;
+                }
+            }
+            catch (Exception genEx)
+            {
+                Console.Clear();
+                Console.WriteLine("There was a problem in the OptionsMenu method");
+                Console.WriteLine(genEx.Message);
+                Console.WriteLine("Please press enter to try again.");
+                Console.ReadLine();
+                throw;
+            }
+        }
+        #endregion
+        #region Create File
+        static void CreateFile(string path)
+        {
+
+            string[] words = { "beginner", "school", "crescendo" };
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (string line in words)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+        #endregion
+        #region Read StreamReader
+        static string[] ReadWordList(string path)
+        {
+            
+
+            string[] wordList = File.ReadAllLines(path);
+
+            return wordList;
+        }
+        #endregion
+        #region Display Word List
+        static void DisplayWordList(string path)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            string[] words = ReadWordList(path);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        #endregion
+        #region Add Word
+        static void AddWord(string path)
+        {
+            Console.WriteLine("Please enter the word that you wish to add.");
+            string word = Console.ReadLine();
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(word);
+            }
+        }
         #endregion
     }
 }
